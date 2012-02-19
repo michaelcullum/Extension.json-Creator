eb59d106cc2a180add91979d613d880d23f28071
Michael Cullum <unknownbliss@phpbbdevelopers.net>
2012-02-19 19:24:36 +0000

    Added inital files, if the phpBB Project wants it once its done I'll change over the copyright lines etc.

---
+++ root/mdcreator.php
@@ -0,0 +1,43 @@
+<?php
+/**
+*
+* @author Michael Cullum (Unknown Bliss of http://michaelcullum.com)
+*
+* @package Meta-data file Creator
+* @copyright (c) 2012 Michael Cullum
+* @license http://opensource.org/licenses/gpl-2.0.php GNU Public License
+*
+*/
+
+/**
+* @ignore
+*/
+define('IN_PHPBB', true);
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+include($phpbb_root_path . 'common.' . $phpEx);
+
+// Start session management
+$user->session_begin();
+$auth->acl($user->data);
+
+// Language
+$user->setup('mdcreator');
+
+// Page title
+page_header($user->lang['MD_CREATOR']);
+
+// Breadcrumbs
+$template->assign_block_vars('navlinks', array(
+            'FORUM_NAME'         => ($user->lang['MD_CREATOR']),
+            'U_VIEW_FORUM'      => append_sid("{$phpbb_root_path}mdcreator.$phpEx"))
+);
+
+$template->set_filenames(array(
+    'body' => 'md_creator.html',
+));
+
+// Display page
+page_footer();
+
+?>
